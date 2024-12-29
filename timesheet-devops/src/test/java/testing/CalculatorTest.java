5d82a47cc46e1414999323492cb889d59fedbf84
ziedtuihri <zied.hack@gmail.com>
2024-12-29 16:04:58 +0100

    JUnit test

---
+++ timesheet-devops/src/test/java/testing/CalculatorTest.java
@@ -0,0 +1,16 @@
+
+package testing;
+
+/**
+ *
+ * @author zied
+ */
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    void test(){
+        fail("Not yet implemented");
+    }
+}
